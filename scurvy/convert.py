1af0ed9eadfb8c6341f36062d5778efb3ee31104
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-01-04 14:02:37 -0800

    Update convert.py

--- scurvy/convert.py
+++ scurvy/convert.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 import numpy.typing as npt
 
 import numpy as np
@@ -10,7 +10,7 @@ def convert_df_to_2d_array(
   x_colname: str, 
   y_colname: str, 
   val_colname: str
-) -> npt.NDArray:
+) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
   
     """
     Converts a dataframe to a 2D array
@@ -19,16 +19,20 @@ def convert_df_to_2d_array(
     :param x_colname: name of table column w/ horizontal coords. (eg longitude)
     :param y_colname: name of table column w/ vertical coords. (eg latitude)
     :param val_colname: name of table column w/ property values (eg rainfall)
-    :return: n_vertical * n_horizontal array of property values 
+    :return: n_vert * n_horiz array of property values, x-coords, y-coords 
     """
     xdim = get_dim_info(df[x_colname])
     ydim = get_dim_info(df[y_colname])
     data = np.full((ydim["even_n_pixels"], xdim["even_n_pixels"]), np.nan)
     for k in range(df.shape[0]):
         i = int(np.round((df[y_colname][k] - ydim["min"]) / ydim["resolution"]))
-        j = int(np.round((df[x_colname][k] - xdim["min"]) / ydim["resolution"]))
+        j = int(np.round((df[x_colname][k] - xdim["min"]) / xdim["resolution"]))
         data[i, j] = df[val_colname][k]
-    return data
+    dx = 0 if xdim["even_n_pixels"] == xdim["n_pixels"] else xdim["resolution"]
+    dy = 0 if ydim["even_n_pixels"] == ydim["n_pixels"] else ydim["resolution"]
+    x = np.linspace(xdim["min"], xdim["max"] + dx, xdim["even_n_pixels"])
+    y = np.linspace(ydim["min"], ydim["max"] + dy, ydim["even_n_pixels"])
+    return data, y, x
 
 
 def get_dim_info(coords1d: npt.NDArray) -> Dict:
