8fa83821c32c1025f6794c4a10ad36e2c8f61d61
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-01-03 22:50:40 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scurvy",
+    version="0.0.1",
+    author="Ricardo Lemos",
+    author_email="rtl00@yahoo.com",
+    description="Space filling curves",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+)
+
