de333afb070ae6ad43cd752f09d37ebd0553685b
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-01-03 23:15:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Space filling curves",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/rtlemos/scurvy/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
