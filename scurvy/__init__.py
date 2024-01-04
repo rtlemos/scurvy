485915898f7673c3a9b729d41135973763fc5bb3
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-01-04 13:15:34 -0800

    Update __init__.py

--- scurvy/__init__.py
+++ scurvy/__init__.py
@@ -1,3 +1,4 @@
+from .convert import convert_df_to_2d_array
 from .preprocess import preprocess
 from .surface_filling_curve import surface_filling_curve
 from .nearest_neighbors import get_nearest_neighbors
