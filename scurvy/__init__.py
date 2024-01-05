a884ddf86cd77b819c235567ca43ecfb933f5c95
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-01-04 20:27:07 -0800

    Update __init__.py

--- scurvy/__init__.py
+++ scurvy/__init__.py
@@ -2,6 +2,6 @@ from .convert import convert_df_to_2d_array
 from .preprocess import preprocess, plot_preprocessing
 from .surface_filling_curve import surface_filling_curve, plot_path
 from .nearest_neighbors import get_nearest_neighbors
-from .sort_df import sort_df, sort_array
+from .sort_df import get_scurvy_index, sort_array
 
 name="scurvy"
