fd18c875b0a70799998822c9d92b5a9320f42bf7
Ricardo T. Lemos <8542576+rtlemos@users.noreply.github.com>
2024-02-18 22:16:14 -0800

    Update convert.py

--- scurvy/convert.py
+++ scurvy/convert.py
@@ -42,8 +42,6 @@ def convert_df_to_2d_array(
     for k in range(df.shape[0]):
         i = int(np.floor((df[y_colname][k] - ydim["min"]) / ydim["resolution"]))
         j = int(np.floor((df[x_colname][k] - xdim["min"]) / xdim["resolution"]))
-        # for originally non-gridded data, the next operation obliterates values
-        # that fall on the same grid point (same i, j)
         data[i, j] += df[val_colname][k]
         cnt[i, j] += 1
     data = np.divide(data, cnt, out=emp, where=cnt > 0)
